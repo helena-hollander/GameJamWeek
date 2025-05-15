f778ceb4da11469ac47520e3c38ed53b3ec0033d
AEllegaard <93321290+AEllegaard@users.noreply.github.com>
2025-05-15 15:11:23 +0200

    ting og sager

--- Assets/Scripts/BoostersScript.cs
+++ Assets/Scripts/BoostersScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class BoostersScript : MonoBehaviour
 {
@@ -8,31 +9,35 @@ public class BoostersScript : MonoBehaviour
     public float jumpSpeed = 0f;
 
     private bool hasJumped = false;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
-    {
-        bouncer = GameObject.FindWithTag("Bouncer");
-        blood = GameObject.FindWithTag("Blood");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-   void OnTriggerEnter(Collider other)
 {
-    if (other.gameObject == player && !hasJumped)
-    {
-        hasJumped = true;
-        Debug.Log("OnTriggerEnter");
-        player.GetComponent<Rigidbody>().AddForce(0, jumpSpeed, 0, ForceMode.VelocityChange);
-        
-        bouncer.transform.localScale = new Vector3(0.5f, 0.05f, 0.5f);
-        blood.transform.localScale = new Vector3(2.5f, 0.02f, 2.5f);
-    }
+    if (player == null)
+        player = GameObject.FindWithTag("Player");
+    bouncer = GameObject.FindWithTag("Bouncer");
+    blood = GameObject.FindWithTag("Blood");
 }
 
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player && !hasJumped)
+        {
+            hasJumped = true;
+            Debug.Log("Boost activated");
+            player.GetComponent<Rigidbody>().AddForce(0, jumpSpeed, 0, ForceMode.VelocityChange);
+
+            bouncer.transform.localScale = new Vector3(0.5f, 0.05f, 0.5f);
+            blood.transform.localScale = new Vector3(2.5f, 0.02f, 2.5f);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            hasJumped = false;
+            Debug.Log("Player left booster, can jump again");
+        }
+    }
 }
